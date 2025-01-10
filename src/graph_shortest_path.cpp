dff003d3d0e75b3a34c33e1caac8406ab10f952a
Ephrem Mekonnen Merid <120575625+Ephrem0001@users.noreply.github.com>
2025-01-10 22:24:09 +0300

    Update graph_shortest_path.cpp

--- src/graph_shortest_path.cpp
+++ src/graph_shortest_path.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 
 using namespace std;
+int main() {
+    int vertices = 5; // Number of vertices
+    Graph graph(vertices);
+
+    // Add edges (u, v, weight)
+    graph.addEdge(0, 1, 2);
+    graph.addEdge(0, 3, 6);
+    graph.addEdge(1, 2, 3);
+    graph.addEdge(1, 3, 8);
+    graph.addEdge(1, 4, 5);
+    graph.addEdge(2, 4, 7);
+    graph.addEdge(3, 4, 9);
+}
