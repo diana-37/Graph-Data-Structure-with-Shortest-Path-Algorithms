39753cf2406eb3a99252affa2fedcce3de12d294
Ephrem Mekonnen Merid <120575625+Ephrem0001@users.noreply.github.com>
2025-01-15 21:23:32 +0300

    Update graph_shortest_path.cpp

--- src/graph_shortest_path.cpp
+++ src/graph_shortest_path.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <climits>
 
 using namespace std;
-int main() {
-    int vertices = 5; // Number of vertices
-    Graph graph(vertices);
+class Graph {
+private:
+    vector<list<pair<int, int>>> adjList; 
+  int vertices;
+public:
+    Graph(int v) : vertices(v), adjList(v) {}
+    void addEdge(int u, int v, int weight) {
+        adjList[u].push_back({v, weight});
+        adjList[v].push_back({u, weight}); 
+    }
 
-    // Add edges (u, v, weight)
-    graph.addEdge(0, 1, 2);
-    graph.addEdge(0, 3, 6);
-    graph.addEdge(1, 2, 3);
-    graph.addEdge(1, 3, 8);
-    graph.addEdge(1, 4, 5);
-    graph.addEdge(2, 4, 7);
-    graph.addEdge(3, 4, 9);
-}
+    void dijkstra(int start) {
+        vector<int> distance(vertices, INT_MAX);
+        distance[start] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, start});
+
+        while (!pq.empty()) {
+            int currentDistance = pq.top().first;
+            int currentVertex = pq.top().second;
+            pq.pop();
+
+            for (auto &neighbor : adjList[currentVertex]) {
+                int neighborVertex = neighbor.first;
+                int edgeWeight = neighbor.second;
+                if (currentDistance + edgeWeight < distance[neighborVertex]) {
+                    distance[neighborVertex] = currentDistance + edgeWeight;
+                    pq.push({distance[neighborVertex], neighborVertex});
+                }
+            }
+        }
+
+        cout << "Shortest distances from vertex " << start << ":\n";
+        for (int i = 0; i < vertices; i++) {
+            cout << "Vertex " << i << ": " 
+                 << (distance[i] == INT_MAX ? "Infinity" : to_string(distance[i])) << endl;
+        }
+    }
+};
+
